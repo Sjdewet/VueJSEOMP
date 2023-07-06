580f2113c9863a8af65576bfe7250c01462c65a0
Sjdewet <summerdewet@gmail.com>
2023-07-06 16:11:44 +0200

    summer change suicide

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,7 @@
 <template>
     <div>
         
-       <p>
-Hello, My name is Summer De Wet , i am 18 years old.
-My home language is English.
-My hobbies include reading, writing poetry, baking, playing soccer and assisting my father in taking apart and rebuilding laptops as well as designing websites.
-My skills include communication, teamwork, problem solving,
-time management, decision making, Bootstrap, HTML and CSS, Javascript and Vue.
-Phone : 0813212836
-Date of Birth: 25/11/2004
-Address: Southfield
-Email: summerdewet@gmail.com</p>
+ 
     </div>
 </template>
 
